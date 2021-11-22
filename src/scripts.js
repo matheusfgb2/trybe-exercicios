3e2247454ec4d1a0baf83661d01dd77cee412a26
Fernanda de Oliveira Carvalho <nandaoc@pop-os.localdomain>
2021-11-22 17:52:09 -0300

    adicionando segundo exercício

--- src/scripts.js
+++ src/scripts.js
@@ -2,14 +2,43 @@ const assert = require('assert');
 const { cities, states, regions } = require('../data/data.js');
 
 /**
- * Dado um arquivo com dados de regiões, estados e cidades, construir uma função (mapCities)
- * que retorna um objeto onde as propriedades são as siglas das regiões. Cada região é um array
- * que possuirá objetos com o nome da cidade e o nome do estado.
- * 
+ * Dado um arquivo com dados de regiões, estados e cidades, crie uma função (mapCities)
+ * que retorna um objeto no qual as chaves são as siglas das regiões (N, NE, CW, SE e S) e os valores
+ * são arrays compostos por objetos com o nome da cidade e o nome do estado.
+ * {
+  N: [
+    { city: 'Manaus', state: 'Amazonas' },
+    { city: 'Belém', state: 'Pará' },
+    { city: 'Porto Nacional', state: 'Tocantins' }
+  ],
+  SE: [
+    { city: 'Lavras', state: 'Minas Gerais' },
+    { city: 'Presidente Prudente', state: 'São Paulo' }
+  ],
+  NE: [
+    { city: 'Feira de Santana', state: 'Bahia' },
+    { city: 'Touros', state: 'Rio Grande do Norte' },
+    { city: 'Jericoacoara', state: 'Ceará' }
+  ],
+  S: [ { city: 'Cascavel', state: 'Paraná' } ]
+}
  */ 
 
 const mapCities = () => {
 
 }
-  
-  
+
+/**
+ * Crie uma função que recebe como parâmetro a sigla de uma região e retorne um array de objetos
+ * contento o nome do estado e o nome da cidade. 
+ * 
+ *[
+    { city: 'Manaus', state: 'Amazonas' },
+    { city: 'Belém', state: 'Pará' },
+    { city: 'Porto Nacional', state: 'Tocantins' }
+* ]
+*/  
+
+const getCitiesByRegion = (region) => {
+
+};
