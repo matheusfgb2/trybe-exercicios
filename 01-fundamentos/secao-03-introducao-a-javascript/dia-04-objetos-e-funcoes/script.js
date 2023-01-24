97629f8e64be7fab9fa5698ba5841e877246a58f
Matheus Boechat <matheusfgb@gmail.com>
2023-01-24 18:38:02 -0300

    exercicios da pagina 'objetos'

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-04-objetos-e-funcoes/script.js
@@ -0,0 +1,14 @@
+let player  = {
+    firstName: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: { golden: 2,
+         silver: 3 },
+};
+
+console.log('A jogadora ' + player.firstName + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade');
+
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+console.log('A jogadora ' + player.firstName + ' ' + player.lastName + ' foi eleita a melhor do mundo por 6 vezes, nos anos de: ' + player['bestInTheWorld']);
+
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata ')
