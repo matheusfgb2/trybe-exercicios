f69ba87a766d4e2c48307122af8ac4d652a68926
Matheus Boechat <matheusfgb@gmail.com>
2023-01-24 19:08:07 -0300

    exercicios da pagina 'for/in e for/of'

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-04-objetos-e-funcoes/script.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-04-objetos-e-funcoes/script.js
@@ -12,3 +12,25 @@ player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
 console.log('A jogadora ' + player.firstName + ' ' + player.lastName + ' foi eleita a melhor do mundo por 6 vezes, nos anos de: ' + player['bestInTheWorld']);
 
 console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata ')
+
+//
+
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+};
+
+for (n4me in names) {
+  console.log("Olá " + names[n4me]);
+}
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (cars in car) {
+  console.log(cars, car[cars]);
+}
