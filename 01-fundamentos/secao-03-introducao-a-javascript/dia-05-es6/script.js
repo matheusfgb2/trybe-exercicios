eb79ad257cab173cae6fed077a413ca499e89572
Matheus Boechat <matheusfgb@gmail.com>
2023-01-27 12:05:26 -0300

    exercicios feitos

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-05-es6/script.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-05-es6/script.js
@@ -1,7 +1,6 @@
 // Parte I
 // 🚀 Altere o tipo das variáveis (var) para let ou const para que respeitem o escopo em que foram declaradas.
 // 🚀 Modifique a variável para que respeite o escopo onde está sendo declarada. Lembre-se: para que a variável respeite o escopo, ela não deve ser acessível fora do escopo em que esteja sendo declarada.
-
 function imprimeIdade() {
   for (let idade = 30; idade <= 40; idade += 1) {
     console.log('Idade dentro do for:', idade)
@@ -40,33 +39,21 @@ console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
 
 // 🚀 Modifique a estrutura das funções a seguir para que elas sejam arrow functions;
 // 🚀 Transforme a função numeroAleatorio em uma arrow function;
-function numeroAleatorio() {
-  return Math.random()
-}
+const numeroAleatorio = () => Math.random();
 console.log(numeroAleatorio());
 
 // 🚀 Transforme a função hello em uma arrow function;
-function hello(nome) {
-  return `Olá, ${nome}!`
-}
+const hello = nome => `Olá, ${nome}!`;
 let nome = 'Ivan';
 console.log(hello(nome));
 
 // 🚀 Transforme a função nomeCompleto em uma arrow function;
-function nomeCompleto(nome, sobrenome) {
-  return `${nome} ${sobrenome}`
-}
+const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`
 let nome = 'Ivan';
 let sobrenome = 'Pires';
 console.log(nomeCompleto(nome, sobrenome));
 
 // 🚀 Altere a expressão if/else utilizando ternary operator;
 let speed = 90;
-const speedCar = (speed) => {
-  if (speed >= 120) {
-    return `Você ultrapassou o limite de velocidade`;
-  } else {
-    return `Você está na velocidade permitida`;
-  }
-};
+const speedCar = (speed) => speed >= 120 ? `Você ultrapassou o limite de velocidade` : `Você está na velocidade permitida`;
 console.log(speedCar(speed));
