006e72ba3ea609814a4555da0ae5b2de8d16ce93
Matheus Boechat <matheusfgb@gmail.com>
2023-01-27 12:45:03 -0300

    exercicios bonus feitos

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-05-es6/script.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-05-es6/script.js
@@ -57,3 +57,42 @@ console.log(nomeCompleto(nome, sobrenome));
 let speed = 90;
 const speedCar = (speed) => speed >= 120 ? `Você ultrapassou o limite de velocidade` : `Você está na velocidade permitida`;
 console.log(speedCar(speed));
+
+//Bônus
+// Função 1: Escreva uma função que substitua a letra ‘x’ em uma frase.
+//// O nome da função deverá ser substituaX;
+//// A função deverá receber um nome por parâmetro;
+//// Declare dentro da função uma variável do tipo const, com o nome frase, atribuindo o valor 'Tryber x aqui!';
+//// A função deverá retornar uma nova frase em que o x da frase 'Tryber x aqui!' seja substituído pelo nome passado por parâmetro.
+// Exemplo:
+//// Parâmetro: ‘Bebeto’
+//// Retorno: ‘Tryber Bebeto aqui!’
+//// Spoiler: O método split() pode ser utilizado de diferentes maneiras, como para separar as palavras de um texto.
+
+const substituaX = nome => {
+  const frase = 'Tryber x aqui!';
+  const fraseArray = frase.split(' ');
+  fraseArray[1] = nome;
+  return fraseArray.join(' ');
+}
+// Função 2: Escreva uma função que irá receber o retorno da Função 1 por parâmetro e retornará uma nova string.
+//// O nome da função deverá ser minhasSkills;
+//// A função deverá receber o retorno da Função 1 - substituaX por parâmetro;
+//// Declare dentro da função uma variável com o nome skills, do tipo const;
+//// A variável skills deverá ser um array contendo três strings com tecnologias que você já aprendeu.
+//// Crie uma varável do tipo let e concatene o valor retornado da Função 1 - substituaX (utilize template literals), a frase 'Minhas três principais habilidades são:' e o valor da variável skills.
+// Exemplo de retorno:
+//// Tryber Bebeto aqui!
+//// Minhas três principais habilidades são:
+//// JavaScript
+//// HTML
+//// CSS
+const minhasSkills = func => {
+  const skills = ['Javascript', 'HTML', 'CSS'];
+  let resultado = `${func}\nMinhas três principais habilidades são:`;
+  for (skill of skills) {
+    resultado += `\n- ${skill}`
+  }
+  return resultado;
+}
+console.log(minhasSkills(substituaX('Didi Mocó')));
