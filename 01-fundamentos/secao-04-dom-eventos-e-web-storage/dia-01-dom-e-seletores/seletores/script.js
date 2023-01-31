3ae766de0f054f3e6fdd4b3cf767cc048700f6b5
Matheus Boechat <matheusfgb@gmail.com>
2023-01-31 15:43:25 -0300

    acertando exercicio qual seletor usar

--- 01-fundamentos/secao-04-dom-eventos-e-web-storage/dia-01-dom-e-seletores/seletores/script.js
+++ 01-fundamentos/secao-04-dom-eventos-e-web-storage/dia-01-dom-e-seletores/seletores/script.js
@@ -1,12 +1,12 @@
 document.getElementById('header-container').style.backgroundColor = 'green';
 
-document.querySelectorAll('.emergency-tasks')[0].style.backgroundColor = 'pink';
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'pink';
 const emergencyTasksH3 = document.querySelectorAll('.emergency-tasks h3');
 for (index of emergencyTasksH3) {
   index.style.backgroundColor = 'purple';
 }
 
-document.querySelectorAll('.no-emergency-tasks')[0].style.backgroundColor = 'yellow';
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'yellow';
 const noEmergencyTasksH3 = document.querySelectorAll('.no-emergency-tasks h3');
 for (index of noEmergencyTasksH3) {
   index.style.backgroundColor = 'black';
