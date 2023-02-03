5d7052fca07ddb7e76370baad1fb771cd7808eb0
Matheus Boechat <matheusfgb@gmail.com>
2023-02-03 19:13:51 -0300

    botoes atribuem propriedades ao texto"

--- 01-fundamentos/secao-04-dom-eventos-e-web-storage/dia-04-web-storage/script.js
+++ 01-fundamentos/secao-04-dom-eventos-e-web-storage/dia-04-web-storage/script.js
@@ -5,6 +5,7 @@
 //  1 - Criar elemento header
 const body = document.querySelector('body');
 const header = document.createElement('header');
+header.id = 'header-container';
 body.appendChild(header);
 //  2 - Criar elemento section com id 'event-lists'
 const headerSection = document.createElement('section');
@@ -33,6 +34,7 @@ createList('font-color-list');
 createList('font-size-list');
 createList('line-height-list');
 createList('font-family-list');
+
 //  5 - Criar itens das listas (criar função createListItem)
 //    1 - Iterar array correspondente
 //    2 - Criar variável com o tipo de elemento 'li' e 'button'
@@ -71,4 +73,58 @@ createListItem(fontFamilyArray, 'font-family-list', 'Tipo da fonte');
 //    1 - display
 //    2 - padding
 //    3 - button
+
 // Parte 2: Criar conteúdo do main
+//  1 - Criar main
+//    1 - Criar section c/ id 'main-title'
+//    2 - Criar título h1 c id 'title'
+//    3 - Criar subtítulo h3 c/ id 'second-title'
+const main = document.createElement('main');
+main.id = 'main-container';
+const mainTitleSection = document.createElement('section');
+mainTitleSection.id = 'main-title';
+const title = document.createElement('h1');
+title.id = 'title';
+title.innerHTML = 'Título';
+const secondTitle = document.createElement('h3');
+secondTitle.id = 'second-title';
+secondTitle.innerHTML = '"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."';
+body.appendChild(main);
+main.appendChild(mainTitleSection);
+mainTitleSection.appendChild(title);
+mainTitleSection.appendChild(secondTitle);
+//  2 - Criar section c/ id 'main-paragraph'
+//    1 - Criar parágrafo
+const mainParagraphSection = document.createElement('section');
+mainParagraphSection.id = 'main-paragraph';
+main.appendChild(mainParagraphSection);
+const mainParagraph = document.createElement('p');
+mainParagraph.innerHTML = '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."';
+mainParagraphSection.appendChild(mainParagraph);
+//  3 - Fazer com que botões disparem um evento
+//    1 - Fazer com que todos os botões sejam escutáveis
+//    2 - Atribuir o estilo, escrito no botão clicado, ao texto
+
+//-------------------------------------------------------
+//Objeto com as classes e seus referentes estilos
+listClasses = {
+  'bg-color-list': 'backgroundColor',
+  'font-color-list': 'color',
+  'font-size-list': 'fontSize',
+  'line-height-list': 'lineHeight',
+  'font-family-list': 'fontFamily'
+};
+const buttons = document.getElementsByClassName('button');
+//Iterando através de todos os botões para captura
+for (button of buttons) {
+  button.addEventListener('click', (event) => {
+    //Variável para estocar a ID da lista atual (pai do pai do evento) 
+    const currentListId = event.target.parentElement.parentElement.id;
+    //Variável para estocar a propriedade atual (ex: black, 8px, etc)
+    const property = event.target.innerHTML;
+    //Conversão do nome da classe para o tipo de estilo (ex: 'bg-color-list' para 'backgroundColor')
+    currentStyleProperty = listClasses[currentListId];
+    //Alteração do estilo na main
+    main['style'][currentStyleProperty] = property;
+  });
+};
