b506ac0f1eb477c5322b6f8efffd9a2027d3513d
Thalles Carneiro <thallescarneiro@gmail.com>
2023-03-02 19:42:03 -0300

    Implementa a função everyTerraqueo

--- 3-everyTerraqueo.js
+++ 3-everyTerraqueo.js
@@ -1,9 +1,14 @@
 const data = require('./database');
 
-// Verifica se todos ou algum personagem é Terráqueo
+// a) Verifica se todos os personagens são Terráqueos
+// b) Verifica se algum personagem é Terráqueo
 
 const everyTerraqueo = () => {
-  
+  const speciesTerraqueo = data.species.find((especie) => especie.name === 'Terráqueo');
+  const terraqueoId = speciesTerraqueo.id;
+
+  return data.characters.every((character) => character.id === terraqueoId);
+  // return data.characters.some((character) => character.id === terraqueoId);
 };
 
 console.log(everyTerraqueo());
