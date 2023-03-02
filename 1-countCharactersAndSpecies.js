4b40cd7f42cc608b13dfbc42eebc410c61049ce1
Thalles Carneiro <thallescarneiro@gmail.com>
2023-03-02 19:40:53 -0300

    Implementa as funções countCharacters e countSpecies

--- 1-countCharactersAndSpecies.js
+++ 1-countCharactersAndSpecies.js
@@ -2,16 +2,12 @@ const data = require('./database');
 
 // Crie uma função que mostre a quantidade de personagens
 
-const countCharacters = () => {
-  
-};
+const countCharacters = () => data.characters.length;
 
 console.log(countCharacters());
 
 // Crie uma função que mostre a quantidade de espécies
 
-const countSpecies = () => {
-  
-};
+const countSpecies = () => data.species.length;
 
 console.log(countSpecies());
