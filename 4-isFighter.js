558f03aef7d2d0b8502133debfe1d0c4b3388a2b
Matheus Boechat <matheusfgb@gmail.com>
2023-03-04 18:49:09 -0300

    exercicio 4

--- 4-isFighter.js
+++ 4-isFighter.js
@@ -2,14 +2,11 @@ const data = require('./database');
 
 // Crie uma função que verifica em todos os personagens se possuem poderes ou não. Então, adiciona a propriedade isFighter com o valor true (para quando tiver poderes) ou false (quando não tiver poderes). Por fim, a função deve retornar o array atualizado.
 
-const isFighter = () => {
-  
-};
-
-console.log(isFighter());
-
 const isFighterForEach = () => {
-  
+  data.characters.forEach(char => {
+    char.isFighter = char.powers.length > 0 && char.powers[0] !== 'Unknown' ? true : false;
+  });
+  return data.characters;
 };
 
 console.log(isFighterForEach());
