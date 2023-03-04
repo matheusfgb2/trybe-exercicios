3c2081a1d9a8dc20b16f47737caad093d8c3c8ad
Matheus Boechat <matheusfgb@gmail.com>
2023-03-04 18:37:45 -0300

    exercicio 3

--- 3-everyTerraqueo.js
+++ 3-everyTerraqueo.js
@@ -1,10 +1,13 @@
 const data = require('./database');
 
+// Função para retornar a ID da espécie pelo nome
+const findSpecieId = (specieName) => {
+  const isASpecie = data.species.some(specie => specie.name === specieName);
+  return isASpecie ? data.species.find(specie => specie.name === specieName).id : Error('Espécie Inválida!');
+}
 // a) Verifica se todos os personagens são Terráqueos
+const everyTerraqueo = () => data.characters.every(char => char.speciesId === findSpecieId('Terráqueo'));
 // b) Verifica se algum personagem é Terráqueo
-
-const everyTerraqueo = () => {
-
-};
-
+const someTerraqueo = () => data.characters.some(char => char.speciesId === findSpecieId('Terráqueo'));
 console.log(everyTerraqueo());
+console.log(someTerraqueo());
