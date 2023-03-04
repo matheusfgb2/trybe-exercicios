f7fd69fbb2ad90de25fca86c2db6623f038fdcc2
Matheus Boechat <matheusfgb@gmail.com>
2023-03-04 19:16:36 -0300

    exercicio 5

--- 5-sortCharactersByName.js
+++ 5-sortCharactersByName.js
@@ -3,7 +3,7 @@ const data = require('./database');
 // Crie uma função que ordene os personagens através do nome
 
 const sortCharactersByName = () => {
-  
-};
+ return data.characters.sort((a, b) => a.name.localeCompare(b.name));
+}
 
 console.log(sortCharactersByName());
