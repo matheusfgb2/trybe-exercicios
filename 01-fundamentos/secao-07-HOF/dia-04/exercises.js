da7b6c3d38e36974d51800df6d7ef77e95deeada
Matheus Boechat <matheusfgb@gmail.com>
2023-03-06 19:56:18 -0300

    exercicios feitos

--- 01-fundamentos/secao-07-HOF/dia-04/exercises.js
+++ 01-fundamentos/secao-07-HOF/dia-04/exercises.js
@@ -2,7 +2,7 @@
 // Dado o código abaixo, altere a chamada da função rectangleArea de forma que seja impressa a área dos 3 retângulos (rectangle1, rectangle2 e rectangle3).
 // O código deve retornar em sequência 2, 15 e 54.
 
-const rectangleArea = (width, height) => width * height;
+const rectangleArea = ([width, height]) => width * height;
 
 const rectangle1 = [1, 2];
 const rectangle2 = [3, 5];
@@ -10,13 +10,14 @@ const rectangle3 = [6, 9];
 const rectangles = [rectangle1, rectangle2, rectangle3];
 
 rectangles.forEach((rectangle) => {
-  console.log(rectangleArea(parametro)); // Altere o parâmetro recebido por rectangleArea()
+  console.log(rectangleArea(rectangle)); // Altere o parâmetro recebido por rectangleArea()
 });
 
 // Exercício 2
 // Crie uma função sum que, dado um número ilimitado de parâmetros, retorna a soma desses parâmetros. Ao chamar a função dessa forma : sum(4,5,6), o número 15 deve ser retornado.
 
-// escreva sum abaixo
+const sum = (...numbers) => numbers.reduce((acc, number) => number + acc);
+console.log(sum(4, 5, 6));
 
 // Exercício 3
 // Escreva a função personLikes, que recebe como parâmetro os objetos alex ou gunnar. Cada objeto representa uma pessoa, e a função deve retornar todos os gostos daquela pessoa, conforme mostrado abaixo:
@@ -37,7 +38,7 @@ const gunnar = {
 };
 
 // complete a assinatura da função abaixo
-const personLikes = () => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
 // <nome> tem <anos de idade> e gosta de <gostos da pessoa>
 
 // Retornos esperados:
@@ -76,6 +77,8 @@ const people = [
 ];
 
 // escreva filterPeople abaixo
+const filterPeople = () => people.filter(({ nationality, bornIn }) => nationality === 'Australian' && (bornIn > 1900 && bornIn < 2001));
+console.log(filterPeople());
 
 // Exercício 5
 // Escreva uma função shipLength que, dado um objeto representando um navio, retorna o comprimento dele, mostrando também a devida unidade de medida:
@@ -100,6 +103,7 @@ const ships = [
 ];
 
 // escreva shipLength abaixo
+const shipLength = ({ name, length, measurementUnit }) => `${name} is ${length} ${measurementUnit}`
 
 // retorno esperado
 console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
@@ -118,3 +122,6 @@ const yearSeasons = {
   winter: ['December', 'January', 'February'],
 };
 // yearSeasons: estações do ano.
+const { spring, summer, autumn, winter } = yearSeasons;
+const months = [ winter[1], winter[2], ...spring, ...summer, ...autumn, winter[0]];
+console.log(months);
