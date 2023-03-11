b9596d1fb6062891cbfd1f9c85db4f184e4a32ef
Matheus Boechat <matheusfgb@gmail.com>
2023-03-11 14:15:58 -0300

    exercicio de form validator

--- 02-fe/01-intro-e-js-assincrono/dia-01/src/main.js
+++ 02-fe/01-intro-e-js-assincrono/dia-01/src/main.js
@@ -1,2 +1,32 @@
 import validator from 'validator';
 import './style.css';
+
+const userInput = document.getElementById('userInput');
+const selectTypeEl = document.getElementById('validatorType');
+const submitBtnEl = document.getElementById('submitBtn');
+const resultEl = document.getElementById('result');
+
+submitBtnEl.addEventListener('click', (event) => {
+  event.preventDefault();
+  const option = selectTypeEl.value;
+  const input = userInput.value;
+  resultEl.innerHTML = 'A avaliação retornou ';
+  switch (option) {
+    case 'letras':
+      resultEl.innerHTML += validator.isAlpha(input);
+      break;
+    case 'email':
+      resultEl.innerHTML += validator.isEmail(input);
+      break;
+    case 'url':
+      resultEl.innerHTML += validator.isURL(input);
+      break;
+    case 'ola':
+      resultEl.innerHTML += validator.equals(input, 'Olá, mundo!');
+      break;
+    case 'par':
+      resultEl.innerHTML += validator.isDivisibleBy(input, 2);
+      break;
+  }
+  
+});
