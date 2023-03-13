b801dcc69775f5480da70302e6df21d261b16a05
Matheus Boechat <matheusfgb@gmail.com>
2023-03-12 21:44:24 -0300

    exercicio superheroes

--- 02-fe/01-intro-e-js-assincrono/dia-02/src/main.js
+++ 02-fe/01-intro-e-js-assincrono/dia-02/src/main.js
@@ -1,7 +1,30 @@
 import Swal from 'sweetalert2';
+import './style.css'
 
-const btn = document.querySelector('button');
+const shCard = document.getElementById('super-hero-card');
+const shImg = document.getElementById('super-hero-img');
+const shName = document.getElementById('super-hero-name');
+const draftBtn = document.getElementById('draft-btn');
 
-btn.addEventListener('click', () => {
-  Swal.fire('Erro');
+const insertHero = (data) => {
+  const randomIndex = Math.floor(Math.random() * data.length - 1);
+  const hero = data[randomIndex];
+  shImg.src = hero.images.md;
+  shImg.alt = hero.slug;
+  shName.innerHTML = hero.name;
+}
+
+draftBtn.addEventListener(('click'), () => {
+  fetch('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json')
+    .then(response => response.json())
+    .then(data => {
+      insertHero(data);
+
+    })
+    .catch(error => {
+      Swal.fire('Personagem não encontrado na base de dados');
+    })
+
 })
+
+
