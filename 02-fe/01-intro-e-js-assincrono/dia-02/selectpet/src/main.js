baf6d77b5b5e517efe5024d42418d9d288fb28bd
Matheus Boechat <matheusfgb@gmail.com>
2023-03-13 12:04:55 -0300

    adicionando pastas para exercicios e iniciando selectpet

---
+++ 02-fe/01-intro-e-js-assincrono/dia-02/selectpet/src/main.js
@@ -0,0 +1,25 @@
+import "./style.css"
+
+const dogBtn = document.getElementById('dog-btn');
+const catBtn = document.getElementById('cat-btn');
+const surpriseBtn = document.getElementById('suprise-btn');
+
+const getRandomDog = (data) => {
+
+}
+
+const getRandomCat = (data) => {
+
+}
+
+dogBtn.addEventListener('click', () => {
+
+})
+
+catBtn.addEventListener('click', () => {
+
+})
+
+surpriseBtn.addEventListener('click', () => {
+
+})
