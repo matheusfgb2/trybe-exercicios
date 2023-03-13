cfe11183ce7414b2180647624d89ae1edd703010
Matheus Boechat <matheusfgb@gmail.com>
2023-03-13 12:52:35 -0300

    finalizando selectpet

--- 02-fe/01-intro-e-js-assincrono/dia-02/selectpet/src/main.js
+++ 02-fe/01-intro-e-js-assincrono/dia-02/selectpet/src/main.js
@@ -2,24 +2,32 @@ import "./style.css"
 
 const dogBtn = document.getElementById('dog-btn');
 const catBtn = document.getElementById('cat-btn');
-const surpriseBtn = document.getElementById('suprise-btn');
+const surpriseBtn = document.getElementById('surprise-btn');
+const img = document.getElementById('pet-img');
 
-const getRandomDog = (data) => {
-
-}
+const getRandomDog = () => {
+fetch('https://dog.ceo/api/breeds/image/random')
+  .then(response => response.json())
+  .then(data => {
+    img.src = data.message;
+  });
+};
 
 const getRandomCat = (data) => {
+  fetch('https://aws.random.cat/meow')
+  .then(response => response.json())
+  .then(data => {
+    img.src = data.file;
+  });
+};
 
-}
+dogBtn.addEventListener('click', () => getRandomDog());
 
-dogBtn.addEventListener('click', () => {
-
-})
-
-catBtn.addEventListener('click', () => {
-
-})
+catBtn.addEventListener('click', () => getRandomCat());
 
 surpriseBtn.addEventListener('click', () => {
-
-})
+Promise.race ([
+  getRandomCat(),
+  getRandomDog(),
+])
+});
